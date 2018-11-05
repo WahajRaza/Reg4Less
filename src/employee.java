0ea3ae8f459969d0336586bc164762dffb8d8e9d
WahajRaza <wahaj_raza12@hotmail.com>
2018-11-05 07:18:44 -0800

    commit first

---
+++ src/employee.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Wajji
+ */
+public class employee {
+    swipecard yes;
+    
+    
+    public void punchcard(swipecard sc){
+    this.yes=sc;
+    
+    }
+}
---
+++ src/swipecard.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Wajji
+ */
+public class swipecard {
+    employee yes;
+    
+    public void recattend(employee e){
+    this.yes=e;
+    
+    }
+     public static void main(String args[]){
+     employee e=new employee();
+     swipecard sc=new swipecard();
+     sc.recattend(e);
+     
+     }
+}
